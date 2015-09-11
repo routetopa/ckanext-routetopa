ca2452b3152466d7f233827fdd89aaeb6db619b8
Mohammad Waqar <mohammad.waqar@deri.org>
2015-09-11 15:33:12 +0100

    minor fix

--- ckanext/routetopa/fanstatic/form.js
+++ ckanext/routetopa/fanstatic/form.js
@@ -1,11 +1,11 @@
 function tetMetadataCompleness() {
 	var inputs = $("#dataset-edit").find("input");
-	var total = inputs.length - 3;
+	var total = inputs.length;
 	var i =0,value =0, result =0;
 	for (i=0; i < total ; i++){
 		if ($(inputs[i]).val() != "") value += 1;
 	}
-	result = Math.round(value/total * 100);
+	result = Math.round(value/(total-4) * 100);
 	$(".bar").css("width", result+"%");
 	$("#field-completeness").val(result)
 }
