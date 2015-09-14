abda31af8e7bb0c8f25fefc608255a4b2fcc9f91
Mohammad Waqar <mohammad.waqar@deri.org>
2015-09-14 11:39:59 +0100

    get TET schema service

--- ckanext/routetopa/plugin.py
+++ ckanext/routetopa/plugin.py
@@ -3,6 +3,9 @@ import ckan.plugins.toolkit as toolkit
 from ckan.lib.base import BaseController
 from logging import getLogger
 from ckan.plugins.toolkit import Invalid
+from ckan.lib.base import BaseController
+from ckan.common import json, response
+import os
 
 log = getLogger(__name__)
 
@@ -20,6 +23,7 @@ def check_tags(value):
 class RoutetopaPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IValidators)
+    plugins.implements(plugins.IRoutes, inherit=True)
 
     # IConfigurer
     def update_config(self, config_):
@@ -30,4 +34,18 @@ class RoutetopaPlugin(plugins.SingletonPlugin):
     # IValidator
     def get_validators(self):
     	return {'check_license': check_license, 'check_tags': check_tags }
-    
+
+    # IRoutes
+    @staticmethod
+    def after_map(m):
+         m.connect('getschema', '/api/3/util/tet/getschema',
+            controller='ckanext.routetopa.plugin:RtpaApi', action='get_schema')
+         return m
+
+class RtpaApi(BaseController):
+    def get_schema(self):
+        response.content_type = 'application/json; charset=UTF-8'
+        path = os.path.dirname(__file__)
+        with open( path + '/tet_dataset.json') as json_file:
+            json_data = json.load(json_file)
+        return json.dumps(json_data)
